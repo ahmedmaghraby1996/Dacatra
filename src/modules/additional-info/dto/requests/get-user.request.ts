b18c844e2fd4a9e8cc3eb3ee7e67e34b9e5bde01
LENOVO <a.maghraby96@yahoo.com>
2024-06-11 10:47:13 +0300

    fixes

--- src/modules/additional-info/dto/requests/get-user.request.ts
+++ src/modules/additional-info/dto/requests/get-user.request.ts
@@ -1,11 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsOptional } from 'class-validator';
-import { IsAdmin } from 'src/core/validators/is-admin.validator';
+
 
 export class GetUserRequest {
   @ApiProperty({ required: false })
   @IsNotEmpty()
   @IsOptional()
-  @IsAdmin({ message: 'User must be an admin to get user' })
+ 
   id: string;
 }
